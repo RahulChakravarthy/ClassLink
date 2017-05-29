6f88659d4ea29612001030cff3a1299f8485e876
Jay Weng <jay.weng7@gmail.com>
2017-05-28 22:46:35 -0400

    testing

--- Source Code/ClassLink/app/src/main/java/app/classlink/mainAppMenu.java
+++ Source Code/ClassLink/app/src/main/java/app/classlink/mainAppMenu.java
@@ -2,6 +2,7 @@ package app.classlink;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 import app.classlink.helperClasses.activityParameters;
 
